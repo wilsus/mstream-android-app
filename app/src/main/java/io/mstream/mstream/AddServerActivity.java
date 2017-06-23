8cbe3b43db49861d41d22a2bdeadb0bcc402a338
IrosTheBeggar <paul.sori@gmail.com>
2017-06-23 11:55:55 -0400

    Chaning the add server activity to handle login

--- app/src/main/java/io/mstream/mstream/AddServerActivity.java
+++ app/src/main/java/io/mstream/mstream/AddServerActivity.java
@@ -1,16 +1,34 @@
 package io.mstream.mstream;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 import io.mstream.mstream.serverlist.ServerItem;
 import io.mstream.mstream.serverlist.ServerStore;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import static io.mstream.mstream.R.string.url;
 
 /**
  * An activity dedicated to adding a server to the app.
@@ -37,6 +55,8 @@ public class AddServerActivity extends AppCompatActivity {
         urlText = (EditText) findViewById(R.id.input_url);
         urlTextLayout = (TextInputLayout) findViewById(R.id.input_url_layout);
         makeDefault = (CheckBox) findViewById(R.id.make_default);
+
+        // Add Server Button
         findViewById(R.id.add_server).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,6 +92,78 @@ public class AddServerActivity extends AppCompatActivity {
             }
         });
 
+        // Test Server Button
+        findViewById(R.id.test_server).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = urlText.getText().toString();
+                String password = passwordText.getText().toString();
+                String username = usernameText.getText().toString();
+
+                if (url.isEmpty() || !Patterns.WEB_URL.matcher(url).matches()) {
+                    toastIt("Invalid URL");
+                    return;
+                }
+
+                Callback loginCallback = new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        toastIt("Failed To Connect To Server");
+
+//                        try {
+//                            //Log.i(TAG, "login failed: " + call.execute().code());
+//                            toastIt("FAILURE");
+//                        } catch (IOException e1) {
+//                            e1.printStackTrace();
+//                        }
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if(response.code() != 200){
+                            toastIt("Login Failed");
+                        }else{
+                            toastIt("Login Success");
+                        }
+
+//                        String loginResponseString = response.body().string();
+//                        try {
+//                            JSONObject responseObj = new JSONObject(response.body().string());
+//                            //Log.i(TAG, "responseObj: " + responseObj);
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+                        // Log.i(TAG, "loginResponseString: " + loginResponseString);
+                    }
+                };
+
+
+                JSONObject jsonObj = new JSONObject();
+                try{
+                    jsonObj.put("username", username);
+                    jsonObj.put("password", password);
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+                okhttp3.RequestBody body = RequestBody.create(JSON, jsonObj.toString());
+
+                String loginURL = Uri.parse(url).buildUpon().appendPath("login").build().toString();
+                Request request = new Request.Builder()
+                        .url(loginURL)
+                        .post(body)
+                        .build();
+
+                OkHttpClient okHttpClient = ((MStreamApplication) getApplicationContext()).getOkHttpClient();
+                okHttpClient.newCall(request).enqueue(loginCallback);
+
+            }
+        });
+
+
         // If there are no servers yet, ensure the Make Default box is checked
         if (ServerStore.getServers().isEmpty()) {
             makeDefault.setChecked(true);
@@ -98,5 +190,15 @@ public class AddServerActivity extends AppCompatActivity {
             urlTextLayout.setError(null);
         }
         return valid;
+    }
+
+
+    private void toastIt(final String toastText){
+        runOnUiThread(new Runnable() {
+            public void run()
+            {
+                Toast.makeText(AddServerActivity.this, toastText, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
